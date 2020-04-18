ffc6bec28f196b592bb3b408ed09d056fcf4aece
Samuel Moses A <56961772+SamuelMosesA@users.noreply.github.com>
2020-04-18 21:22:29 +0530

    commented out cuda

--- 3-ML-Basic.sh
+++ 3-ML-Basic.sh
@@ -25,48 +25,48 @@ run_and_echo () {
 # For utilities such as lspci
 execute sudo apt-get install pciutils
 
-if [[ (-n $(lspci | grep -i nvidia)) && (! ( -d "/usr/local/cuda" ) ) ]]; then
-    echo "Installing the latest cuda"
-    cuda_instr_block=$(wget -q -O - 'https://developer.nvidia.com/cuda-downloads' | grep wget | head -n 1)
-    cuda_download_command=$(echo ${cuda_instr_block} | sed 's#\(.*\)"cudaBash">.*#\1#' | sed 's#.*"cudaBash">\([^<]*\).*#\1#' )
-    cuda_install_command=$(echo ${cuda_instr_block} | sed 's#.*"cudaBash">\([^<]*\).*#\1#' | sed 's#&nbsp;# ./extras/#' ) # Get everything after the last `"cudaBash"` block till the next `<` character, replace the &nbsp; with a download path
-    if [[ $(command -v aria2c) ]]; then
-        cuda_download_command=$(echo ${cuda_download_command} | sed 's#wget#aria2c --file-allocation=none -c -x 10 -s 10 --dir extras#' )
-    else
-        cuda_download_command=$(${cuda_download_command} -P ./extras)
-    fi
+# if [[ (-n $(lspci | grep -i nvidia)) && (! ( -d "/usr/local/cuda" ) ) ]]; then
+#     echo "Installing the latest cuda"
+#     cuda_instr_block=$(wget -q -O - 'https://developer.nvidia.com/cuda-downloads' | grep wget | head -n 1)
+#     cuda_download_command=$(echo ${cuda_instr_block} | sed 's#\(.*\)"cudaBash">.*#\1#' | sed 's#.*"cudaBash">\([^<]*\).*#\1#' )
+#     cuda_install_command=$(echo ${cuda_instr_block} | sed 's#.*"cudaBash">\([^<]*\).*#\1#' | sed 's#&nbsp;# ./extras/#' ) # Get everything after the last `"cudaBash"` block till the next `<` character, replace the &nbsp; with a download path
+#     if [[ $(command -v aria2c) ]]; then
+#         cuda_download_command=$(echo ${cuda_download_command} | sed 's#wget#aria2c --file-allocation=none -c -x 10 -s 10 --dir extras#' )
+#     else
+#         cuda_download_command=$(${cuda_download_command} -P ./extras)
+#     fi
 
-    $cuda_download_command
-    execute $cuda_install_command --silent --toolkit --run-nvidia-xconfig
-fi
+#     $cuda_download_command
+#     execute $cuda_install_command --silent --toolkit --run-nvidia-xconfig
+# fi
 
 
-if [[ (! -n $(echo $PATH | grep 'cuda')) && ( -d "/usr/local/cuda" ) ]]; then
-    echo "Adding Cuda location to PATH"
-    run_and_echo "# Cuda" $SHELLRC
-    run_and_echo "export PATH=/usr/local/cuda/bin:\$PATH" $SHELLRC
-    run_and_echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:\$LD_LIBRARY_PATH" $SHELLRC
-    run_and_echo "export CUDA_HOME=/usr/local/cuda" $SHELLRC
-fi
+# if [[ (! -n $(echo $PATH | grep 'cuda')) && ( -d "/usr/local/cuda" ) ]]; then
+#     echo "Adding Cuda location to PATH"
+#     run_and_echo "# Cuda" $SHELLRC
+#     run_and_echo "export PATH=/usr/local/cuda/bin:\$PATH" $SHELLRC
+#     run_and_echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:\$LD_LIBRARY_PATH" $SHELLRC
+#     run_and_echo "export CUDA_HOME=/usr/local/cuda" $SHELLRC
+# fi
 
-if which nvidia-smi > /dev/null; then 
-    echo "Installing nvtop"
-    execute sudo apt-get install cmake libncurses5-dev git -y
-    if [[ ! -d "nvtop" ]]; then
-        execute git clone --quiet https://github.com/Syllo/nvtop.git
-    else
-    (
-        cd nvtop || exit
-        execute git pull origin master
-    )
-    fi
-    execute mkdir -p nvtop/build
-    cd nvtop/build
-    execute cmake -DCMAKE_BUILD_TYPE=Optimized -DNVML_RETRIEVE_HEADER_ONLINE=True ..
-    execute make
-    execute sudo make install
-    cd ../../
-fi
+# if which nvidia-smi > /dev/null; then 
+#     echo "Installing nvtop"
+#     execute sudo apt-get install cmake libncurses5-dev git -y
+#     if [[ ! -d "nvtop" ]]; then
+#         execute git clone --quiet https://github.com/Syllo/nvtop.git
+#     else
+#     (
+#         cd nvtop || exit
+#         execute git pull origin master
+#     )
+#     fi
+#     execute mkdir -p nvtop/build
+#     cd nvtop/build
+#     execute cmake -DCMAKE_BUILD_TYPE=Optimized -DNVML_RETRIEVE_HEADER_ONLINE=True ..
+#     execute make
+#     execute sudo make install
+#     cd ../../
+# fi
 
 if [[ $(command -v conda) || (-n $CIINSTALL) ]]; then
     PIP="pip install"
